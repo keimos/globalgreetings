73a5fef4bfde59b5c9565ebecc38eceb442d300d
keimos <keithmosleyjr@gmail.com>
2021-06-25 21:49:55 -0400

    Added RestController for present JSON details

--- src/main/kotlin/com/keimos/globalgreetings/GlobalgreetingsApplication.kt
+++ src/main/kotlin/com/keimos/globalgreetings/GlobalgreetingsApplication.kt
@@ -2,6 +2,8 @@ package com.keimos.globalgreetings
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RestController
 
 @SpringBootApplication
 class GlobalgreetingsApplication
@@ -9,3 +11,17 @@ class GlobalgreetingsApplication
 fun main(args: Array<String>) {
 	runApplication<GlobalgreetingsApplication>(*args)
 }
+
+@RestController
+class MessageResource {
+	@GetMapping
+	fun index(): List<Message> = listOf(
+		Message("1", "Hello!"),
+		Message("2", "Hola!"),
+		Message("3", "Bonjour!"),
+		Message("4", "Privet!"),
+	)
+}
+
+
+data class Message (val id: String?, val text: String)
